12823064e00c40a27d2f2d6aec2ae9bdb902522a
mindfigment <maksymilian.szmelczynski@gmail.com>
2019-02-11 12:22:20 +0100

    Second submission, LB: 0.753, epochs: 400

---
+++ code/files/val_generator.py
@@ -0,0 +1,32 @@
+from keras.utils import Sequence
+from keras import backend as K
+from utils import read_raw_image
+import numpy as np
+
+class ValData(Sequence):
+    def __init__(self, validation, batch_size=32):
+        """
+        @param steps: number of epoch we are planning with this score matrix
+        """
+        super(ValData, self).__init__()
+        self.validation = validation
+        self.batch_size = batch_size
+        self.img_shape = (384,384,1)
+
+    def __getitem__(self, index):
+        start = self.batch_size * index
+        end = min(start + self.batch_size, len(self.validation))
+        size = end - start
+        assert size > 0
+        a = np.zeros((size, ) + self.img_shape, dtype=K.floatx())
+        b = np.zeros((size, ) + self.img_shape, dtype=K.floatx())
+        c = np.zeros((size,1), dtype=K.floatx())
+
+        for i in range(size):
+            a[i,:,:,:] = read_raw_image(self.validation[i][0])
+            b[i,:,:,:] = read_raw_image(self.validation[i][1])
+            c[i,:] = self.validation[i][2]
+        return [a,b], c
+
+    def __len__(self):
+        return (len(self.validation) + self.batch_size - 1) // self.batch_size
